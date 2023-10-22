42025fc6e811f33abd8349909ee17256dc2f85de
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2023-10-22 19:16:55 +0200

    Create server configuration file

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Application } from 'express';
+import cors, { CorsOptions } from 'cors';
+
+export class Server {
+    constructor(app: Application) {
+        this.config(app);
+    }
+
+    private config(app: Application) {
+        const corsOptions: CorsOptions = {
+            origin: 'https://localhost:8000',
+        };
+
+        app.use(cors(corsOptions));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+    }
+}
