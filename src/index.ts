d26c9c9b234cbb355cd249abd2547dd127a5bc0c
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2023-10-22 19:58:17 +0200

    404 not found

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express';
 import { Server } from './server';
 import { PORT } from './config/port';
 
 const app: Application = express();
 const server: Server = new Server(app);
 
-app.get('/test', (req, res) => {
-    res.send({ status: 'oki doki' });
-});
-
 app.listen(PORT, () =>
     console.log(`Server is started and listening the port ${PORT}`)
 ).on('error', (err) => console.error(err));
+
+/** Error handling */
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message,
+    });
+});
